01d9fdc85c1db63bb4c751ef9158522cc70ad65e
ujusixoffizial <ujusix@gmail.com>
2022-04-03 22:21:00 +0200

    test 3+4

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+    }
+}
