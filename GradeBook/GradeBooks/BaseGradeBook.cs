c601235114ad8c53ecdd8b1abb18e6aa8643a133
ujusixoffizial <ujusix@gmail.com>
2022-04-03 18:22:51 +0200

    test 2.1

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -20,6 +20,9 @@ namespace GradeBook.GradeBooks
             Students = new List<Student>();
         }
 
+        public GradeBookType Type { get; set; }
+
+
         public void AddStudent(Student student)
         {
             if (string.IsNullOrEmpty(student.Name))
